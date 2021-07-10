a7829ecccab009d2794b504ab32b9cf0530985c2
Firez <55920870+FirezTheGreat@users.noreply.github.com>
2021-07-10 13:13:05 +0530

    Initialise

---
+++ structures/models/applicants.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require('mongoose');
+
+const MyApplicantSchema = new Schema({
+    ApplicantID: {
+        type: String,
+        required: true,
+        index: {
+            unique: true
+        }
+    },
+    SubmitDate: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = model('Applicants', MyApplicantSchema, 'Applicants');
